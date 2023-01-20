5162c4ce70e119369e561ab4d73287f194fe5560
Sheikh Bayazid <sheikh.bayazid@shortcut.no>
2023-01-20 16:27:14 +0100

    Fix typo

--- Sources/ShortcutPersistence/IPersistence.swift
+++ Sources/ShortcutPersistence/IPersistence.swift
@@ -16,29 +16,29 @@ public typealias IPersistence = ILocalPersistence
 
 /// Main interface and entry point to consume the library
 ///
-/// Exampel usage:
+/// Example usage:
 ///
 ///     class Cat: IPersistenceModel {
 ///         let age: Int?
 ///     }
 ///
 ///     let persistence: IPersistence
-///     let cat = persistence.get(Cat.sef)
+///     let cat = persistence.get(Cat.self)
 ///
 public protocol ILocalPersistence {
     /// Returns a  storage object by a generic type.
-    /// In that case we will reaturn only one element if they will exist. If there will be more
+    /// In that case we will return only one element if they will exist. If there will be more
     /// it will be returned first matched item
     ///
     /// - Parameter objectType: Type of object what we want to fetch
-    /// - Parameter storage: source of data paersistence it can be `keychain`, `sqlite`, `realm`
+    /// - Parameter storage: source of data persistence it can be `keychain`, `sqlite`, `realm`
     /// - Returns: A generic type  which conforms to `PersistenceModelInterface` protocol
     func get<T: IPersistenceModel>(_ objectType: T.Type, storage: LocalSupportedStorage) -> T?
 
     /// Returns a storage object by generic type and index (primary key).
     ///
     /// - Parameter objectType: Type of object what we want to fetch
-    /// - Parameter storage: source of data paersistence it can be `keychain`, `sqlite`, `realm`
+    /// - Parameter storage: source of data persistence it can be `keychain`, `sqlite`, `realm`
     /// - Parameter index: Index of the object you want to retrieve
     /// - Returns: A generic type  which conforms to `PersistenceModelInterface` protocol
     func get<T: IPersistenceModel>(_ objectType: T.Type, storage: LocalSupportedStorage, index: String) -> T?
@@ -46,7 +46,7 @@ public protocol ILocalPersistence {
     /// Returns an array of storage objects
     ///
     /// - Parameter objectType: Type of object what we want to fetch
-    /// - Parameter storage: source of data paersistence it can be `keychain`, `sqlite`
+    /// - Parameter storage: source of data persistence it can be `keychain`, `sqlite`
     /// - Returns: A generic type array  which conforms to `PersistenceModelInterface` protocol
     func get<T: IPersistenceModel>(arrayOf objectType: T.Type, storage: LocalSupportedStorage) -> [T]
     func save<T: IPersistenceModel>(_ object: inout T, storage: LocalSupportedStorage) throws
